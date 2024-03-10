6c23aac763ef4cdceaaba05a528bf7351e2eafd0
matthewacarson <github-carsonm@snkmail.com>
2024-03-09 16:25:29 -0800

    Updated analysis
    Created mind maps
    updated script to include Excel tables and new plots

--- 6_analyze_typologies.R
+++ 6_analyze_typologies.R
@@ -1,19 +1,22 @@
 rm(list = ls())
-# Load libraries ----------------------------------------------------------
+# Load libraries ----------------------------------------------
 library(sf)
 library(tidyverse)
+conflicted::conflicts_prefer(dplyr::filter)
+conflicted::conflicts_prefer(dplyr::filter)
 library(reticulate)
 library(jmv)
+library(openxlsx)
 
-# Load data ---------------------------------------------------------------
+# Load data ---------------------------------------------------
 
-## UDP typologies from python pickle ----------------------------------
+## UDP typologies from python pickle --------------------------
 
 udp_typologies <-
   py_load_object(paste0(getwd(), '/data/pickle_files/final_typology_output.pkl'))
 
 
-### Save udp typologies to RData ---------------------------------------
+### Save udp typologies to RData ------------------------------
 
 # save(
 #   udp_typologies,
@@ -23,11 +26,14 @@ udp_typologies <-
 # )
 # )
 
-## Load udp typologies from RData ---------------------------------------
+## Load udp typologies from RData -----------------------------
 
-# load(paste0(getwd(), '/data/outputs/typologies/final_typology_output.RData'))
+# load(
+#   paste0(
+#     getwd(), 
+#     '/data/outputs/typologies/final_typology_output.RData'))
 
-### Fatal encounters with 2019 GEOID (joined) -------------------------------
+### Fatal encounters with 2019 GEOID (joined) -----------------
 
 load(file = paste0(getwd(), '/data/R_data/fatal_enc_2019.RData'))
 
@@ -51,52 +57,52 @@ fatal_enc_2019 <- joined_2019 |>
       )
   ) |> filter(race_imputed != "Other/Unknown")
 
-### All census tracts -- 2019 data -------------------------------------
+### All census tracts -- 2019 data ----------------------------
 
 load(file = paste0(getwd(), '/data/R_data/all_tracts_2019.RData'))
 
-#### Rename all_tracts_2019 ------------------------------------------------
+#### Rename all_tracts_2019 -----------------------------------
 
 all_tracts_2019 <- income_population_quintiles_2019
-
-# Condense UDP categories -------------------------------------------------
+rm(income_population_quintiles_2019)
+# Condense UDP categories -------------------------------------
 # There are currently too many categories -- combine
 
 udp_typologies <- udp_typologies |>
   mutate(
     'UDP Typology' = case_when(
       # "LIR", # Low-income or at-risk
-      typology %in% c('LISD', 'OD', 'ARG') ~ 'L-income/At-risk',
+      typology %in% c('LISD', 'ARG') ~ 'L-income/At-risk',
       # 'GIP', # Gentrification in progress
-      typology %in% c('EOG', 'AdvG') ~ 'Gentrifying',
-      # "MHIS", # Stable: mixed or high-income
-      typology %in% c('ARE', 'BE', 'SAE', 'SMMI') ~ 'Stable',
+      typology %in% c('EOG', 'OD', 'BE') ~ 'Gentrifying',
+      # "SMOHI", # Stable: mixed or high-income
+      typology %in% c('ARE', 'SAE', 'SMMI', 'AdvG') ~ 'Stable',
       
       TRUE ~ NA
-    ),
-    
-    typology = case_when(
-      # "LIR", # Low-income or at-risk
-      typology %in% c('LISD', 'OD', 'ARG') ~ 'LIR',
-      # ' Low-income or at-risk',
-      # 'GIP', # Gentrification in progress
-      typology %in% c('EOG', 'AdvG') ~ ' GIP',
-      # 'Gentrification in progress',
-      # "MHIS", # Stable: mixed or high-income
-      typology %in% c('ARE', 'BE', 'SAE', 'SMMI') ~ 'MHIS',
-      # 'Stable: mixed or high-income',
-      TRUE ~ NA
     )
+    
+    # ,typology = case_when(
+    #   # "LIR", # Low-income or at-risk
+    #   typology %in% c('LISD', 'OD', 'ARG') ~ 'LIR',
+    #   # ' Low-income or at-risk',
+    #   # 'GIP', # Gentrification in progress
+    #   typology %in% c('EOG', 'AdvG', 'SMMI') ~ 'GIP',
+    #   # 'Gentrification in progress',
+    #   # "SMOHI", # Stable: mixed or high-income
+    #   typology %in% c('ARE', 'BE', 'SAE') ~ 'SMOHI',
+    #   # 'Stable: mixed or high-income',
+    #   TRUE ~ NA
+    # )
   )
 
 
-### Make typologies ordered -------------------------------------------------
+### Make typologies ordered -----------------------------------
 
 typology_order <- c("L-income/At-risk",
                     "Gentrifying",
                     "Stable")
 
-## Remove geometries since I can join with GEOIDs/FIPS ---------------------
+## Remove geometries since I can join with GEOIDs/FIPS --------
 
 fatal_enc_2019 <- st_drop_geometry(fatal_enc_2019)
 udp_typologies <- st_drop_geometry(udp_typologies)
@@ -104,9 +110,9 @@ all_tracts_2019 <- st_drop_geometry(all_tracts_2019)
 
 # Combining -----------------------------------------------
 
-## Add UDP to 2019 census data ---------------------------------------------
+## Add UDP to 2019 census data --------------------------------
 
-### Columns to keep ---------------------------------------------------------
+### Columns to keep -------------------------------------------
 
 keep_udp_cols <- c(
   'GEOID',
@@ -309,7 +315,7 @@ keep_udp_cols <- c(
 )
 
 
-### Join --------------------------------------------------------------------
+### Join ------------------------------------------------------
 
 all_tracts_2019 <-
   left_join(x = all_tracts_2019,
@@ -317,47 +323,47 @@ all_tracts_2019 <-
             by = join_by(GEOID))
 
 
-## Add LUOF counts to all_tracts_2019 --------------------------------------
+## Add LUOF counts to all_tracts_2019 -------------------------
 
-### Creating LUOF by GEOID frequency table -------------------------------
+### Creating LUOF by GEOID frequency table --------------------
 
 GEOID_count <- table(fatal_enc_2019$GEOID) |> as.data.frame() |>
-  rename(GEOID = Var1, luof_count = Freq) |>
+  rename(GEOID = Var1, luof_count = Freq)
+
+### Join frequency table with all_tracts_2019 -----------------
+
+all_tracts_2019 <-
+  left_join(all_tracts_2019,
+            GEOID_count) |> 
   mutate(luof_count =
            case_when(is.na(luof_count) ~ 0,
                      TRUE ~ luof_count))
 
-### Join frequency table with all_tracts_2019 ------------------------------
-
-all_tracts_2019 <-
-  left_join(all_tracts_2019,
-            GEOID_count)
-
-## Bring UDP typologies into fatal encounters df --------------------------
+## Bring UDP typologies into fatal encounters df --------------
 
 fatal_enc_2019_udp <-
   left_join(x = fatal_enc_2019,
             y = all_tracts_2019)
 
 
-# Summarizing -------------------------------------------------------------
+# Summarizing -------------------------------------------------
 
-## Tables: by UDP typology only --------------------------------------------
+## Tables: by UDP typology only -------------------------------
 
-### Population by UDP typology ---------------------------------------------
+### Population by UDP typology --------------------------------
 
 pop_udp_only <-
   aggregate(pop_18 ~ `UDP Typology`, data = all_tracts_2019, FUN = sum)
 
 
-### LUOF by UDP typology ---------------------------------------------------
+### LUOF by UDP typology --------------------------------------
 
 luof_by_udp <-
   table(fatal_enc_2019_udp$`UDP Typology`) |> as.data.frame() |>
   rename('UDP Typology' = Var1, luof_count = Freq)
 
 
-### Combine 'pop_udp_only' and 'LUOF_by_UDP' ------------------------------
+### Combine 'pop_udp_only' and 'LUOF_by_UDP' ------------------
 
 summary_udp_luof <-
   full_join(luof_by_udp,
@@ -368,32 +374,31 @@ summary_udp_luof <-
 summary_udp_luof$`UDP Typology` <-
   factor(summary_udp_luof$`UDP Typology`, levels = typology_order)
 
-#### Plot --------------------------------------------------------------------
+#### Plot -----------------------------------------------------
 
-ggplot(summary_udp_luof,
-       aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`)) +
-  geom_col(color = 'black', fill = '#a6c4f1') +
-  geom_text(
-    aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
-    position = position_dodge(width = 0.85),
-    vjust = -0.4,
-    color = "black",
-    size = 3
-  ) +
-  labs(title = "Rate of Police Lethal Uses of Force",
-       # subtitle = "Years: [2015-2020]",
-       y = "Annual Rate Per 10 Million",
-       x = "Urban Displacement Project Typology",
-       # "Based on Median Household Income in Census Tracts Where a Lethal Use of Force Occurred",
-       fill = "Majority") +
-  ylim(0, 65) +
-  theme_classic() +
-  theme(
-    axis.text.x = element_text(color = "black"),
-    axis.text.y = element_text(color = "black"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+# ggplot(summary_udp_luof,
+#        aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`)) +
+#   geom_col(color = 'black', fill = '#a6c4f1') +
+#   geom_text(
+#     aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
+#     position = position_dodge(width = 0.85),
+#     vjust = -0.4,
+#     color = "black",
+#     size = 3
+#   ) +
+#   labs(title = "Rate of Police Lethal Uses of Force",
+#        # subtitle = "Years: [2015-2020]",
+#        y = "Annual Rate Per 10 Million",
+#        x = "Urban Displacement Project Typology",
+#        fill = "Majority") +
+#   ylim(0, 65) +
+#   theme_classic() +
+#   theme(
+#     axis.text.x = element_text(color = "black"),
+#     axis.text.y = element_text(color = "black"),
+#     panel.grid.major.x = element_blank(),
+#     panel.grid.minor.x = element_blank()
+#   )
 
 descriptives(
   data = summary_udp_luof,
@@ -411,18 +416,22 @@ descriptives(
   barCounts = T
 )
 
-# ggsave(
-#   'plots/udp_only.png',
-#   dpi = 'retina',
-#   width = 10.4,
-#   height = 4.81
-# )
+ggsave(
+  'plots/udp_only.png',
+  dpi = 'retina',
+  width = 10.4,
+  height = 4.81
+)
 
-write_csv(x = summary_udp_luof[, c('UDP Typology', 'luof_count', 'Annual Rate Per 10 Million Population')], file = "summary_udp_luof.csv")
+wb <- createWorkbook()
 
-## Rate by Majority & UDP typology -----------------------------------------
+addWorksheet(wb, "UDP Only")
+writeData(wb, "UDP Only", summary_udp_luof, startCol = 1, startRow = 1)
+# write_csv(x = summary_udp_luof, file = "summary_udp_luof.csv", )
 
-### Population by UDP and Majority race ------------------------------------
+## Rate by Majority & UDP typology ----------------------------
+
+### Population by UDP and Majority race -----------------------
 
 pop_udp_majority <- all_tracts_2019 |>
   aggregate(NH_WhiteE ~ `UDP Typology`, FUN = sum, data = _) |>
@@ -440,7 +449,7 @@ pop_udp_majority <- all_tracts_2019 |>
   )
 
 
-### LUOF count by typology & Majority --------------------------------------
+### LUOF count by typology & Majority -------------------------
 
 
 luof_by_udp_majority <-
@@ -452,7 +461,7 @@ luof_by_udp_majority <-
 
 
 
-### Join population and LUOF count -----------------------------------------
+### Join population and LUOF count ----------------------------
 
 summary_udp_majority_luof <-
   left_join(
@@ -460,8 +469,7 @@ summary_udp_majority_luof <-
     pop_udp_majority,
     by = join_by(`UDP Typology`, Majority)
   ) |>
-  mutate('Annual Rate Per 10 Million Population' = luof_count / population / 6 * 10000000) |>
-  select(`UDP Typology`, Majority, 'Annual Rate Per 10 Million Population')
+  mutate('Annual Rate Per 10 Million Population' = luof_count / population / 6 * 10000000)
 
 
 summary_udp_majority_luof$`UDP Typology` <-
@@ -472,38 +480,38 @@ summary_udp_majority_luof$Majority <-
   factor(summary_udp_majority_luof$Majority, ordered = TRUE)
 
 
-##### Plot -------------------------------------------------------------------
+##### Plot ----------------------------------------------------
 
-ggplot(summary_udp_majority_luof,
-       aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`, fill = Majority)) +
-  geom_col(position = 'dodge', color = 'black') +
-  geom_text(
-    aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
-    position = position_dodge(width = 0.85),
-    vjust = -0.4,
-    color = "black",
-    size = 3
-  ) +
-  labs(title = "Rate of Police Lethal Uses of Force",
-       # subtitle = "Years: [2015-2020]",
-       y = "Annual Rate Per 10 Million",
-       x = "Urban Displacement Project Typology",
-       # "Based on Median Household Income in Census Tracts Where a Lethal Use of Force Occurred",
-       fill = "Majority") +
-  ylim(0, 65) +
-  theme_classic() +
-  theme(
-    axis.text.x = element_text(color = "black"),
-    axis.text.y = element_text(color = "black"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+# ggplot(summary_udp_majority_luof,
+#        aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`, fill = Majority)) +
+#   geom_col(position = 'dodge', color = 'black') +
+#   geom_text(
+#     aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
+#     position = position_dodge(width = 0.85),
+#     vjust = -0.4,
+#     color = "black",
+#     size = 3
+#   ) +
+#   labs(title = "Rate of Police Lethal Uses of Force",
+#        # subtitle = "Years: [2015-2020]",
+#        y = "Annual Rate Per 10 Million",
+#        x = "Urban Displacement Project Typology",
+#        fill = "Majority") +
+#   ylim(0, 65) +
+#   theme_classic() +
+#   theme(
+#     axis.text.x = element_text(color = "black"),
+#     axis.text.y = element_text(color = "black"),
+#     panel.grid.major.x = element_blank(),
+#     panel.grid.minor.x = element_blank()
+#   )
 
 
 descriptives(
   data = summary_udp_majority_luof,
   vars = "Annual Rate Per 10 Million Population",
   splitBy = vars("UDP Typology", "Majority"),
+  mean = F,
   median = F,
   min = F,
   max = F,
@@ -515,28 +523,31 @@ descriptives(
   barCounts = TRUE
 )
 
-# ggsave(
-#   'plots/udp_majority.png',
-#   dpi = 'retina',
-#   width = 10.4,
-#   height = 4.81)
+ggsave(
+  'plots/udp_majority.png',
+  dpi = 'retina',
+  width = 10.4,
+  height = 4.81)
 
-write_csv(x = summary_udp_majority_luof,
-          file = 'summary_udp_majority_luof.csv')
+addWorksheet(wb, "UDP & Majority")
+writeData(wb, "UDP & Majority", summary_udp_majority_luof, startCol = 1, startRow = 1)
+
+# write_csv(x = summary_udp_majority_luof,
+          # file = 'summary_udp_majority_luof.csv')
 
 ######################################################################## #
 ## Tables: Rate by victim race and UDP typology ------------------------------------
 
 # Using pop_udp_majority for population counts
 
-### LUOF count --------------------------------------------------------------
+### LUOF count ------------------------------------------------
 
 luof_udp_victim_race <- fatal_enc_2019_udp |>
   count(`UDP Typology`, race_imputed) |>
   rename(Victim = race_imputed, luof_count = n) |>
   na.omit()
 
-### Join population & LUOF count tables -------------------------------------
+### Join population & LUOF count tables -----------------------
 
 summary_udp_victim_race <-
   left_join(
@@ -551,37 +562,37 @@ summary_udp_victim_race$`UDP Typology` <-
          ordered = T)
 summary_udp_victim_race$Victim <-
   factor(summary_udp_victim_race$Victim, ordered = T)
-#### Plot --------------------------------------------------------------------
+#### Plot -----------------------------------------------------
 
-ggplot(summary_udp_victim_race,
-       aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`, fill = Victim)) +
-  geom_col(position = 'dodge', color = 'black') +
-  geom_text(
-    aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
-    position = position_dodge(width = 0.85),
-    vjust = -0.4,
-    color = "black",
-    size = 3
-  ) +
-  labs(title = "Rate of Police Lethal Uses of Force",
-       # subtitle = "Years: [2015-2020]",
-       y = "Annual Rate Per 10 Million",
-       x = "Urban Displacement Project Typology",
-       # "Based on Median Household Income in Census Tracts Where a Lethal Use of Force Occurred",
-       fill = "Victim's Race") +
-  ylim(0, 100) +
-  theme_classic() +
-  theme(
-    axis.text.x = element_text(color = "black"),
-    axis.text.y = element_text(color = "black"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+# ggplot(summary_udp_victim_race,
+#        aes(x = `UDP Typology`, y = `Annual Rate Per 10 Million Population`, fill = Victim)) +
+#   geom_col(position = 'dodge', color = 'black') +
+#   geom_text(
+#     aes(label = round(`Annual Rate Per 10 Million Population`, 1)),
+#     position = position_dodge(width = 0.85),
+#     vjust = -0.4,
+#     color = "black",
+#     size = 3
+#   ) +
+#   labs(title = "Rate of Police Lethal Uses of Force",
+#        # subtitle = "Years: [2015-2020]",
+#        y = "Annual Rate Per 10 Million",
+#        x = "Urban Displacement Project Typology",
+#        fill = "Victim's Race") +
+#   ylim(0, 100) +
+#   theme_classic() +
+#   theme(
+#     axis.text.x = element_text(color = "black"),
+#     axis.text.y = element_text(color = "black"),
+#     panel.grid.major.x = element_blank(),
+#     panel.grid.minor.x = element_blank()
+#   )
 
 descriptives(
   data = summary_udp_victim_race,
   vars = "Annual Rate Per 10 Million Population",
   splitBy = vars("UDP Typology", "Victim"),
+  mean = F,
   median = F,
   min = F,
   max = F,
@@ -593,19 +604,23 @@ descriptives(
   
 )
 
-# ggsave(
-#   'plots/udp_victim_race.png',
-#   dpi = 'retina',
-#   width = 10.4,
-#   height = 4.81)
+ggsave(
+  'plots/udp_victim_race.png',
+  dpi = 'retina',
+  width = 10.4,
+  height = 4.81)
 
-write_csv(x = summary_udp_victim_race,
-          file = 'summary_udp_victim_race.csv')
+addWorksheet(wb, "UDP & Victim Race")
+writeData(wb, "UDP & Victim Race", summary_udp_victim_race, startCol = 1, startRow = 1)
 
-######################################################################## #
-## Tables: UDP & Majority race & victim race -------------------------------
 
-### Population by racial group in UDP & Majority tract ----------------------
+# write_csv(x = summary_udp_victim_race,
+          # file = 'summary_udp_victim_race.csv')
+
+############################################################# #
+## Tables: UDP & Majority race & victim race ------------------
+
+### Population by racial group in UDP & Majority tract ------
 # For example, the number of blacks living in Majority-white, gentrifying tracts
 
 
@@ -634,7 +649,7 @@ pop_udp_majority_victim <- all_tracts_2019 |>
     values_to = "population"
   )
 
-### LUOF count by UDP & Majority race & victim race -------------------------
+### LUOF count by UDP & Majority race & victim race ---------
 
 
 # table(
@@ -649,8 +664,8 @@ luof_udp_majority_victim <- fatal_enc_2019_udp |>
   rename(Victim = race_imputed, luof_count = n)
 
 
-### Join population & LUOF count tables -------------------------------------
-conflicted::conflicts_prefer(dplyr::filter)
+### Join population & LUOF count tables ---------------------
+
 summary_udp_majority_victim_luof <-
   full_join(luof_udp_majority_victim, pop_udp_majority_victim) |>
   dplyr::filter(Victim != "Other/Unknown") |>
@@ -670,6 +685,7 @@ descriptives(
   data = summary_udp_majority_victim_luof,
   vars = "Annual Rate Per 10 Million Population",
   splitBy = vars("UDP Typology", "Victim", "Majority"),
+  mean = F,
   median = F,
   min = F,
   max = F,
@@ -689,7 +705,13 @@ ggsave(
 )
 
 # print(summary_udp_majority_victim_luof |> arrange('Annual Rate Per 10 Million Population'), n = 50)
-write_csv(summary_udp_majority_victim_luof, file = 'summary_udp_majority_victim_luof_original.csv')
 
-#### Plot: made in Minitab ---------------------------------------------------
+addWorksheet(wb, "UDP, Victim & Majority")
+writeData(wb, "UDP, Victim & Majority", summary_udp_majority_victim_luof, startCol = 1, startRow = 1)
+
+# write_csv(summary_udp_majority_victim_luof, file = 'summary_udp_majority_victim_luof_original.csv')
+
+saveWorkbook(wb, "all_udp_summary_tables.xlsx", overwrite = TRUE)
+
+#### Plot: made in Minitab ----------------------------------
 
